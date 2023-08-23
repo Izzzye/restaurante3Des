4e1381bc5ee3e77044bd9f3611fef3df6aff69e1
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-08-23 08:05:04 -0300

    mobile motoboy

---
+++ appmotoboy/App.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/home';
+import PedidosScreen from './src/screens/pedidos';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ title: 'Delivery' }}
+        />
+        <Stack.Screen
+          name="PedidosScreen"
+          component={PedidosScreen}
+          options={{ title: 'Entregas' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
